41a938b675428a1187838dff36ec78c807ce5c7b
Erno <ernovenalainen@hotmail.com>
2020-10-06 07:10:43 +0300

    Exercise 10.2: setting up the ESLint

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Muutetaan renderöitävää tekstiä!!!.</Text>
+      <Text>I just added linting thing!</Text>
       <StatusBar style="auto" />
     </View>
   );
