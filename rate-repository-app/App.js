bbae55a97a7838057095e86ae0c3b096d27857d3
Erno <ernovenalainen@hotmail.com>
2020-10-08 18:25:43 +0300

    pushataan muutokset repoon

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/react-hooks';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 import Main from './src/components/Main';
 
 const App = () => {
   return (
     <NativeRouter>
-      <Main />
+      <ApolloProvider client={apolloClient}>
+        <Main />
+      </ApolloProvider>
     </NativeRouter>
   );
 };
